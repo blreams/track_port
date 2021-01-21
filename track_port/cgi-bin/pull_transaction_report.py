21a0d468ad60c9115774fcba73638bef4930d7cc
Byron Reams <blreams@gmail.com>
2021-01-21 10:50:50 -0500

    debug

--- track_port/cgi-bin/pull_transaction_report.py
+++ track_port/cgi-bin/pull_transaction_report.py
@@ -476,7 +476,10 @@ class TransactionList(object):
         """This is where we tot up columns. The only columns we total are
         Day, MktVal, Gain, Port% and Basis. We also calculate Day% and Gain%.
         """
-        daypct = Decimal(100.0) * self.daygain / self.totalvalue
+        try:
+            daypct = Decimal(100.0) * self.daygain / self.totalvalue
+        except:
+            assert False, f"help me, {self.fileportname}"
         daycolor = calc_bgcolor(daypct, 0.1, 5.0)
         gainpct = Decimal(100.0) * self.realized_gain / self.totalvalue
         gaincolor = calc_bgcolor(gainpct, 1.0, 50.0)
